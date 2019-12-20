5e18c5254309a05b551928b9c62bcd8d4a3e448b
Alexander Römer <alex@roemer.design>
2019-12-20 16:21:02 +0100

    Initial Commit

---
+++ AppleWatchBreathing WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  AppleWatchBreathing WatchKit Extension
+//
+//  Created by Alexander Römer on 20.12.19.
+//  Copyright © 2019 Alexander Römer. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
