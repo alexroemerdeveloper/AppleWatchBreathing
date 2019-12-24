03f0d0841ec1811ec34c485db226e18af9ce7c58
Alexander Römer <alex@roemer.design>
2019-12-24 10:06:26 +0100

    Initial commit

--- AppleWatchBreathing WatchKit Extension/ContentView.swift
+++ AppleWatchBreathing WatchKit Extension/ContentView.swift
@@ -9,8 +9,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var rotate_in_out = false
+    @State private var scale_in_out  = false
+    
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            Group {
+                ZStack {
+                    Circle().frame(width: 80, height: 80).foregroundColor(.blue).offset(y: -42)
+                    Circle().frame(width: 80, height: 80).foregroundColor(.blue).offset(y: 42)
+                }
+            }.opacity(1/3)
+            
+            Group {
+                ZStack {
+                    Circle().frame(width: 80, height: 80).foregroundColor(.blue).offset(y: -42)
+                    Circle().frame(width: 80, height: 80).foregroundColor(.blue).offset(y: 42)
+                }.rotationEffect(.degrees(60)).opacity(1/4)
+            }
+            
+            Group {
+                ZStack {
+                    Circle().frame(width: 80, height: 80).foregroundColor(.blue).offset(y: -42)
+                    Circle().frame(width: 80, height: 80).foregroundColor(.blue).offset(y: 42)
+                }.rotationEffect(.degrees(120)).opacity(1/4)
+            }
+        }
+        .rotationEffect(.degrees(rotate_in_out ? 90 : 0))
+        .scaleEffect(rotate_in_out ? 1 : 1/8)
+        .animation(Animation.easeInOut.repeatForever(autoreverses: true).speed(1/8))
+        .onAppear() {
+            self.rotate_in_out.toggle()
+            self.scale_in_out.toggle()
+        }
     }
 }
 
